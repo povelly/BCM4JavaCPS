73082b30d0a286a4a8bb77582b72a9772a7f7f27
3682130 <3682130@ppti-14-407-02.ufr-info-p6.jussieu.fr>
2020-02-03 15:10:34 +0100

    First commi

---
+++ src/components/publisher/Publisher.java
@@ -0,0 +1,12 @@
+package components.publisher;
+
+import fr.sorbonne_u.components.AbstractComponent;
+
+public class Publisher extends AbstractComponent {
+
+	protected Publisher(int nbThreads, int nbSchedulableThreads) {
+		super(nbThreads, nbSchedulableThreads);
+		// TODO Auto-generated constructor stub
+	}
+
+}
